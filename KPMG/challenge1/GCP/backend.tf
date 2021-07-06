70c5a76b5e58c05346009f39af22ae32f106cb34
pankajakhade <44667391+pankajakhade@users.noreply.github.com>
2021-07-06 17:50:33 +0530

    Update backend.tf

--- KPMG/challenge1/GCP/backend.tf
+++ KPMG/challenge1/GCP/backend.tf
@@ -1,6 +1,6 @@
 /*terraform {
   backend "gcs" {
-    bucket  = "ecl-terraform-state-bucket"
-    prefix  = "terraform/app/<deployment-date>"
+    bucket  = "terraform-state-bucket"
+    prefix  = "terraform/"
   }
 }*/
